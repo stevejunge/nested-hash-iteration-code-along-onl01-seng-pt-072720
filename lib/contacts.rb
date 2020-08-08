8df4a3bdda69dd81ee34f041beece08987da94f1
Stephen Junge <steve.junge@gmail.com>
2020-08-08 04:02:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -32,7 +32,6 @@ contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       binding.pry
-  
-end
-
+    end
+  end
 end
